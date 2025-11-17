35dd7a8abde9bab1b0c60cdc33a56f778173df94
ChristopherTyler0 <ctyler2@ufl.edu>
2025-11-17 10:26:40 -0500

    created a character generative transformer and a function in completion.py which uses it for text scanning and improvement

--- backend/completion/completion.py
+++ backend/completion/completion.py
@@ -1,33 +1,49 @@
+import tensorflow as tf
+import numpy as np
+import modelCreation
+model = tf.keras.models.load_model('generator.keras')
+model.compile()
+sequenceLength = modelCreation.getSeqLen()
 
-def encode(encodee):
-    encoded = []
-    for char in encodee:
-        encoded.append(ord(char))
-    return encoded
-
-def decode(decodee):
-    decoded = ''
-    for int in decodee:
-        decoded += chr(int)
-    return decoded
-
-with open('trainingData/latinCorpus.txt', 'r', encoding='utf-8') as f:
-    tempText = f.read()
-
-text = ""
-#clean text data
-for char in tempText:
-    if not char.isnumeric():
-        if char.isalpha() or char == ' ' or char == '\n':
-            text += char
+def test():
+    testSeq = "s"
+    testSeq = modelCreation.sequenceToInputFormat(testSeq)
+    probDist = model(testSeq)[0]
+    threshold = 1/32
+    for i in range(0,len(probDist)):
+        if probDist[i] >= threshold:
+            print(f"{modelCreation.decode(i)}: {probDist[i]}")
 
 
-vocab = sorted(list(set(text)))
+def textCorrection(text, threshold):
+    #iterate over whole text
+    for i in range(1,len(text) - 1):
+        sequence = modelCreation.sequenceToInputFormat('')
+        if(i < sequenceLength):
+            threshold = (threshold * sequenceLength) / i
+            sequence = modelCreation.sequenceToInputFormat(text[:i])
+        else:
+            sequence = modelCreation.sequenceToInputFormat(text[i-sequenceLength:i])
+        probDist = model(sequence)[0]
 
-encoded = encode(text)
-testText = decode(encoded)
+        charsInThreshold = []
+        maxProb = 0.0
+        maxChar = ' '
 
-if(text != testText):
-    print("encode/decode failed")
-else:
-    print("encode/decode succeeded")
+        for j in range(0,len(probDist)):
+            probability = probDist[j]
+            char = modelCreation.decode(j)
+            if (probability >= threshold):
+                charsInThreshold.append(char)
+                if(probability > maxProb):
+                    maxProb = probability
+                    maxChar = char
+        if text[j] not in charsInThreshold:
+            text = text[:j] + maxChar + text[j+1:]
+    return text
+
+print(textCorrection("sacra, quae Cronia esse iterantur ab illis, eumque diem celebrant per agros urbesque fere omnes exercent epulas laeti famulosque procurant quisque suos", 1/32))
+
+
+
+
